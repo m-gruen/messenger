b4f98150bff879dbe463399f418cb56ff55f0acc
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-13 12:32:42 +0100

    chore(docker): setup docker-compose environment
    includes:
    - postgres database
    - express backend
    - vue + vite frontend

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.get('/', (_, res) => {
     res.send('success');
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`running on http://localhost:${process.env.PORT}`);
+app.listen(process.env.BACKEND_PORT, () => {
+    console.log(`running on http://localhost:${process.env.BACKEND_PORT}`);
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
+if (!process.env.FRONTEND_PORT) {
+  throw new Error('FRONTEND_PORT is not set')
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: parseInt(process.env.FRONTEND_PORT),
+    strictPort: true,
+  },
 })
