468d9642d333d37f87822681ebafb9bc7b38216b
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-03-27 11:28:47 +0100

    feat(backend): implemented contactRouter for displaying the contacts in json format

--- backend/routes/contact.ts
+++ backend/routes/contact.ts
@@ -6,7 +6,16 @@ import { getContacts } from '../utilities/contact';
 export const contactRouter = Router();
 
 
-contactRouter.get('/', async (_ , res) =>{
-   
+contactRouter.get('/:id', async (req, res) =>{
+   const id = req.params.id;
+   // Validation beeing done in the get Contacts class
+   try {
+      const contacts = await getContacts(parseInt(id,10));
+
+      res.status(StatusCodes.OK).json(contacts)
+   } catch (error) {
+      res.status(StatusCodes.BAD_REQUEST).json({"Error" : "There was and error while getting the data form the DB : " + error})
+   } 
+
 });
 
