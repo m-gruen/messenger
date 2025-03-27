aa2f2f3d83caae6a3cb5e3b9554b73dd65b22887
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-03-27 13:55:59 +0100

    feat(backend, frontend): enhance static file serving

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp({
+  setup() {
+    return {
+      count: ref(0)
+    }
+  }
+}).mount('#App')
