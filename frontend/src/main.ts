54e986ee5ae16ac8b491205b913f396f8560a2bf
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-03-28 08:25:31 +0100

    feat(frontend): configure TypeScript paths, add Tailwind CSS setup, and create utility functions and added Shadcn Compontent for vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css' // Ensure this file exists for global styles
+import './style.css' 
+import path from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import { defineConfig } from 'vite'
 
-// Create and mount the Vue application
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
+
 createApp(App).mount('#app')
