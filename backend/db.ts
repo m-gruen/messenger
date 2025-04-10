5f3c6f5d71b9a93b0afdf88b827c5120e299c9b3
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-10 09:40:45 +0200

    fix(user): enforce unique usernames

--- backend/db.ts
+++ backend/db.ts
@@ -88,7 +88,7 @@ export class DbSession {
             const statements = [
                 `CREATE TABLE IF NOT EXISTS account (
                     uid SERIAL PRIMARY KEY,
-                    username VARCHAR(255) NOT NULL,
+                    username VARCHAR(255) NOT NULL UNIQUE,
                     password_hash VARCHAR(255) NOT NULL,
                     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
                 )`,
